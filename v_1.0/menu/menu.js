28d6bcd62a1ca5bd0f0d843d3c69ad1785026e60
Francesco Tardanico <Francesco Tardanico>
2017-02-25 00:10:01 +0100

    .

---
+++ v_1.0/menu/menu.js
@@ -0,0 +1,39 @@
+var menu = [
+    {
+        id: "dashboard",
+        name: "Dashboard",
+        link: "home/home.html"
+    },
+    {
+        id: "fatture",
+        name: "Fatturazione",
+        link: "#",
+        rows:[
+            {
+                id: "new-fattura",
+                name: "Nuova Fattura",
+                link: "scadenzario/create.html"
+            },
+            {
+                id: "list-fattura",
+                name: "Nuova Fattura",
+                link: "scadenzario/list.html"
+            }
+        ]
+    },
+    {
+        id: "clienti",
+        name: "Clienti",
+        link: "#"
+    },
+    {
+        id: "profilo",
+        name: "Profilo",
+        link: "#"
+    },
+    {
+        id: "logout",
+        name: "Esci",
+        link: "logout/logout.html"
+    },
+];
